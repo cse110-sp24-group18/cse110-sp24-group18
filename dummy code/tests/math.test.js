bfab9f03ef0163a8e2cefaf86afc387b0dbc8e9d
Michael Cheung Ka Ming <59136711+michaelcheungkm@users.noreply.github.com>
2024-05-09 19:50:38 -0700

    Update math.test.js

--- dummy code/tests/math.test.js
+++ dummy code/tests/math.test.js
@@ -1,7 +1,7 @@
 const { add, subtract } = require('../math');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(4);
 });
 
 test('subtracts 5 - 3 to equal 2', () => {
