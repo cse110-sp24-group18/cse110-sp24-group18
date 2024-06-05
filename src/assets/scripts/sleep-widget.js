d66506843a1cd38988c42920dabba58f13c143b8
Taiki-id31 <tayoshino@ucsd.edu>
2024-06-04 17:24:42 -0700

    sleep-widget documentation

--- src/assets/scripts/sleep-widget.js
+++ src/assets/scripts/sleep-widget.js
@@ -1,11 +1,18 @@
 import { updateChecked } from './widgets.js';
 
+/**
+ * This initializes and manages the sleep-widget.
+ */
 document.addEventListener('DOMContentLoaded', function() {
   const slider = document.querySelector('.slider');
+
+  // Updates the slider background color,
   slider.oninput = function() {
     const percentage = computePercentage(this.value);
     this.style.background = `linear-gradient(to right, #FF5C35 0%, #FF5C35 ${percentage}%, #e5dedb ${percentage}%, #e5dedb 100%)`;
   };
+
+  //set default slider position
   slider.value = 3;  
   slider.dispatchEvent(new Event('input')); 
   
@@ -13,6 +20,9 @@ document.addEventListener('DOMContentLoaded', function() {
   //chanage a label color
   const condition_labels = document.querySelectorAll('.label p');
   const time_labels = document.querySelectorAll('.label span');
+
+  //remove heighlight from all labels, and apply the style to
+  //only selected label.
   function updateLabelColor() {
     condition_labels.forEach(label => {
       label.classList.remove('highlight-color');
@@ -21,13 +31,18 @@ document.addEventListener('DOMContentLoaded', function() {
       label.classList.remove('highlight-color');
     });
     condition_labels[5 - (slider.value)].classList.add('highlight-color');
-    updateChecked('sleep', condition_labels[5 - (slider.value)].textContent);
     time_labels[5 - (slider.value)].classList.add('highlight-color');
+    updateChecked('sleep', condition_labels[5 - (slider.value)].textContent);
   }
   updateLabelColor();
   slider.addEventListener('input', updateLabelColor);
 });
 
+/**
+ * Updates label color, slider position, and slider background color,
+ * corresponding to the currentSleep state.
+ * @param {int} currentSleep 
+ */
 export function setSleep(currentSleep) {
   
   //set slider position
@@ -52,6 +67,11 @@ export function setSleep(currentSleep) {
   time_labels[5 - sliderValue].classList.add('highlight-color');
 }
 
+/**
+ * Computes the percentage of the slider's background color gauge.
+ * @param {int} sliderValue 
+ * @returns associated value between 0 and 100
+ */
 export function computePercentage(sliderValue) {
   return (sliderValue - 1) * 25;
 }
