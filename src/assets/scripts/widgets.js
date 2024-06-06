5278f3acb2c46de686b66bec9cee59aa7edae516
BernicoJC <108056736+BernicoJC@users.noreply.github.com>
2024-06-06 01:23:17 -0700

    Revert "Large Commit with a lot of UI fixes, widget functionality inclusion, and HTML changes"

--- src/assets/scripts/widgets.js
+++ src/assets/scripts/widgets.js
@@ -1,8 +1,3 @@
-import { returnChecked, writeFile, readFile } from "./fileSys.js";
-import { linesOfCodeListeners } from './lines-of-code-script.js';
-import { emotionWidgetListeners } from "./emotion-widget.js";
-import { summaryInit } from "./summary.js";
-
 export function widgetButtonListeners() {
   const widgetButtons = document.querySelectorAll('.widget-btn');
 
@@ -13,7 +8,6 @@ export function widgetButtonListeners() {
 
     btn.addEventListener('click', () => {
       const targetWidget = document.getElementById(btn.dataset.target);
-      console.log(targetWidget.id);
 
       if (targetWidget) {
         // If widget is closed, open
@@ -22,7 +16,7 @@ export function widgetButtonListeners() {
           widgetButtons.forEach((i) => {
             const widgetTemp = document.getElementById(i.dataset.target);
             widgetTemp.style.display = 'none';
-            widgetTemp.style.right = '-17em';
+            widgetTemp.style.right = '-30em';
             i.style.filter = 'brightness(1)';
           }); 
           targetWidget.style.display = 'flex';
@@ -32,42 +26,10 @@ export function widgetButtonListeners() {
 
         else {
           targetWidget.style.display = 'none';
-          targetWidget.style.right = '-17em';
+          targetWidget.style.right = '-30em';
           btn.style.filter = 'brightness(1)';
         }
-
       }
     });
   });
-
-  linesOfCodeListeners();
-  emotionWidgetListeners();
-  summaryInit();
-}
-
-export function updateChecked(widget, value, test = false) {
-  if(test) return;
-  const journalDate = returnChecked();
-  console.log(journalDate);
-  let journalEntry = readFile(journalDate);
-
-  switch (widget) {
-    case 'emotion':
-      console.log('emotion widget set', value);
-      journalEntry['mood'] = value;
-      break;
-    case 'sleep':
-      console.log('sleep widget set', value);
-      journalEntry['sleep'] = value;
-      break;
-    case 'linesCoded':
-      console.log('lines coded widget set', value);
-      journalEntry['linesCoded'] = value;
-      break;
-    default:
-      console.log('error, defaulting on widget', value);
-      break;
-  }
-  
-  writeFile(journalEntry, journalDate);
 }
